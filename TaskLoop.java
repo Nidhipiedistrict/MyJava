50c7c4647f42896e29495188bdb8e60c49456f01
NidheeshPiedistrict <nidheesh@piedistrict.com>
2018-05-25 18:53:22 +0530

    Tutorial2

---
+++ TaskLoop.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class TaskLoop {
+
+	public static void main(String[] args) {
+		
+		int a = 50;
+		
+		for(int i = 0;i<=50;i++){
+			
+			if(i%5 == 0){
+				
+				System.out.println(i);
+			}
+		}
+	}
+
+}
