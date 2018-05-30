6ae9f10a54fb5b886a4ba4cd0b69a793a52081fc
NidheeshPiedistrict <nidheesh@piedistrict.com>
2018-05-30 18:56:31 +0530

    MathFunction

---
+++ MathFunction.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class MathFunction {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		System.out.println("square = "+ Math.pow(a, 2));
+		System.out.println("squareroot = "+ Math.sqrt(a));
+		System.out.println("cube = "+ Math.pow(a, 3));
+
+	}
+
+}
